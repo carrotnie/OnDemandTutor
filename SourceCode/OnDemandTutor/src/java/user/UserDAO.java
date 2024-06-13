f92b220cd98732dc9f0f1ce5829a2f64eb601eda
NoBugIfNoCode <143237301+NoBugIfNoCode@users.noreply.github.com>
2024-06-13 13:09:52 +0700

    adminjsp vs search

--- SourceCode/OnDemandTutor/src/java/user/UserDAO.java
+++ SourceCode/OnDemandTutor/src/java/user/UserDAO.java
@@ -5,12 +5,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class UserDAO {
     private static final String LOGIN = "SELECT Id, Name, Role FROM Account WHERE Username=? AND Password=?";
     private static final Logger LOGGER = Logger.getLogger(UserDAO.class.getName());
+    private static final String SEARCH = "SELECT Id, Name, Username, Password, Role FROM Account WHERE Name LIKE ? OR Role LIKE ?";
 
     public UserDTO checkLogin(String Username, String Password) throws SQLException {
         UserDTO user = null;
@@ -45,5 +48,39 @@ public class UserDAO {
             if (conn != null) conn.close();
         }
         return user;
+    }
+
+    public List<UserDTO> getAllUsers(String search) throws SQLException {
+        List<UserDTO> users = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(SEARCH);
+                ptm.setString(1, "%" + search + "%");
+                ptm.setString(2, "%" + search + "%");
+                rs = ptm.executeQuery();
+                while (rs.next()) {
+                    int Id = rs.getInt("Id");
+                    String Name = rs.getString("Name");
+                    String Username = rs.getString("Username");
+                    String Password = rs.getString("Password");
+                    String Role = rs.getString("Role");
+                    users.add(new UserDTO(Id, Name, Username, Password, Role));
+                }
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error at UserDAO: " + e.toString());
+        } finally {
+            if (rs != null) rs.close();
+            if (ptm != null) ptm.close();
+            if (conn != null) conn.close();
+        }
+        return users;
     }
 }
